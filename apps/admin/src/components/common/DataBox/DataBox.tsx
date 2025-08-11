eea46a0c715c86b6577233a98c112591abab7059
MinjuN07 <2minjun07@gmail.com>
2025-08-11 15:18:51 +0900

    feat(admin): DataBox Toggle 확장

--- apps/admin/src/components/common/DataBox/DataBox.tsx
+++ apps/admin/src/components/common/DataBox/DataBox.tsx
@@ -1,27 +1,54 @@
+import { useState } from 'react';
 import { color } from '@maru/design-system';
-import { Text } from '@maru/ui';
+import { IconArrowBottom } from '@maru/icon';
+import { Row, Text } from '@maru/ui';
 import { flex } from '@maru/utils';
 import styled from 'styled-components';
 
 type LengthType = 'SHORT' | 'LONG';
+type ViewType = 'NORMAL' | 'TOGGLE';
 
 interface DataBoxProps {
   label: string;
   data: string | number;
   lengthType?: LengthType;
+  ViewType?: ViewType;
 }
 
-const DataBox = ({ label, data, lengthType = 'SHORT' }: DataBoxProps) => {
+const DataBox = ({
+  label,
+  data,
+  lengthType = 'SHORT',
+  ViewType = 'NORMAL',
+}: DataBoxProps) => {
+  const [isOpen, setIsOpen] = useState(ViewType === 'NORMAL');
+
+  const handleToggle = () => {
+    if (ViewType === 'TOGGLE') {
+      setIsOpen((prev) => !prev);
+    }
+  };
+
   return (
     <StyledDataBox lengthType={lengthType}>
-      <Text fontType="H4" color={color.gray900}>
-        {label}
-      </Text>
-      <DataUnderlineBox lengthType={lengthType}>
-        <Text fontType="p2" color={color.gray900}>
-          {data}
+      <Row width="100%" justifyContent="space-between" alignItems="center">
+        <Text fontType="H4" color={color.gray900}>
+          {label}
         </Text>
-      </DataUnderlineBox>
+        {ViewType === 'TOGGLE' && (
+          <ArrowWrapper isOpen={isOpen} onClick={handleToggle}>
+            <IconArrowBottom width={24} height={24} color={color.gray600} />
+          </ArrowWrapper>
+        )}
+      </Row>
+
+      {isOpen && (
+        <DataUnderlineBox lengthType={lengthType}>
+          <Text fontType="p2" color={color.gray900}>
+            {data}
+          </Text>
+        </DataUnderlineBox>
+      )}
     </StyledDataBox>
   );
 };
@@ -44,4 +71,11 @@ const DataUnderlineBox = styled.div<{ lengthType: LengthType }>`
   width: ${(props) => (props.lengthType === 'SHORT' ? '60%' : '100%')};
   padding-bottom: 4px;
   border-bottom: 1px solid ${color.gray200};
+`;
+
+const ArrowWrapper = styled.div<{ isOpen: boolean }>`
+  display: flex;
+  transform: rotate(${(props) => (props.isOpen ? '180deg' : '0deg')});
+  transition: transform 0.2s ease-in-out;
+  cursor: pointer;
 `;
