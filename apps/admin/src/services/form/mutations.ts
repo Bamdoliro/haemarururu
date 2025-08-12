b6464e60154bf56b560764a3593df141bf01cbc8
MinjuN07 <2minjun07@gmail.com>
2025-08-12 18:00:45 +0900

    feat(admin): 변경된 2차 점수 엑셀 업로드 방식 적용

--- apps/admin/src/services/form/mutations.ts
+++ apps/admin/src/services/form/mutations.ts
@@ -17,12 +17,46 @@ export const useUploadSecondScoreFormatMutation = (handleCloseModal: () => void)
   const { handleError } = useApiError();
   const queryClient = useQueryClient();
 
+  const downloadBlob = (blob: Blob, filename: string) => {
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    window.URL.revokeObjectURL(url);
+  };
+
   const { mutate: uploadSecondScoreFormat, ...restMutation } = useMutation({
     mutationFn: (formData: FormData) => patchSecondScoreFormat(formData),
-    onSuccess: () => {
-      toast('파일이 입력되었습니다.', { type: 'success' });
-      queryClient.invalidateQueries({ queryKey: [KEY.FORM_LIST] });
-      handleCloseModal();
+    onSuccess: async (res) => {
+      const contentType = res.headers['content-type'] ?? '';
+      const blob = res.data as Blob;
+
+      try {
+        if (contentType.includes('application/json')) {
+          const { message } = await blob.text().then(JSON.parse);
+          toast.error(message || '잘못된 파일입니다.');
+          return;
+        }
+        switch (res.status) {
+          case 204:
+            toast.success('파일이 입력되었습니다.');
+            queryClient.invalidateQueries({ queryKey: [KEY.FORM_LIST] });
+            break;
+
+          case 400:
+            downloadBlob(blob, '점수입력_오류결과.xlsx');
+            toast.error('오류가 있는 파일이 다운로드되었습니다.');
+            break;
+
+          default:
+            toast.error('알 수 없는 응답입니다.');
+        }
+      } finally {
+        handleCloseModal();
+      }
     },
     onError: handleError,
   });
