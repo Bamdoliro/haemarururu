56fcd0ac41ba319047e685dc423c766aa2215e81
oxjadex <oharin765@gmail.com>
2025-08-17 22:22:18 +0900

    fix(admin): question 없을 때 모달 안 열리도록 수정

--- apps/admin/src/services/fair/mutations.ts
+++ apps/admin/src/services/fair/mutations.ts
@@ -3,15 +3,19 @@ import type { FairApiRequestBody } from '@/components/fair/FairForm/fair.hooks';
 import { postFairReq } from '@/services/fair/api';
 import { useApiError } from '@/hooks';
 import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
 
 export const useCreateFairMutation = () => {
   const { handleError } = useApiError();
+  const router = useRouter();
+
   return useMutation({
     mutationFn: (data: FairApiRequestBody) => postFairReq(data),
     onSuccess: () => {
       toast.success('입학 설명회 일정이 만들어졌습니다.', {
         type: 'success',
       });
+      router.push('/fair');
     },
     onError: handleError,
   });
