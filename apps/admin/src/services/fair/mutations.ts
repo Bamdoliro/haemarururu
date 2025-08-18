d69bcda2bec17104a3d1fd4fae5b2ee8dcac9fb1
junghongseop <a01082372487@gmail.com>
2025-08-18 21:28:24 +0900

    fix(admin): router 에러 수정

--- apps/admin/src/services/fair/mutations.ts
+++ apps/admin/src/services/fair/mutations.ts
@@ -3,7 +3,8 @@ import type { FairApiRequestBody } from '@/components/fair/FairForm/fair.hooks';
 import { postFairReq } from '@/services/fair/api';
 import { useApiError } from '@/hooks';
 import { toast } from 'react-toastify';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
+import { ROUTES } from '@/constants/common/constant';
 
 export const useCreateFairMutation = () => {
   const { handleError } = useApiError();
@@ -15,7 +16,7 @@ export const useCreateFairMutation = () => {
       toast.success('입학 설명회 일정이 만들어졌습니다.', {
         type: 'success',
       });
-      router.push('/fair');
+      router.push(ROUTES.FAIR);
     },
     onError: handleError,
   });
