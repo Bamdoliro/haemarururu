f8baa94fee9cb51be17b1bd52ac336d5cdb8c184
hjy080530 <bomaye119@gmail.com>
2025-09-03 01:08:13 +0900

    feat(user): 토스트 추가

--- apps/user/src/components/form/Contents/IntroductionContent/IntroductionContent.hook.ts
+++ apps/user/src/components/form/Contents/IntroductionContent/IntroductionContent.hook.ts
@@ -4,6 +4,7 @@ import { useFormStore, useSetFormGradeStepStore, useSetFormStepStore } from '@/s
 import { useFormStep } from '@/utils';
 import { useState, type ChangeEventHandler } from 'react';
 import { z } from 'zod';
+import { useToast } from '@/hooks';
 
 export const useIntoductionForm = () => {
   const [form, setForm] = useFormStore();
@@ -12,6 +13,7 @@ export const useIntoductionForm = () => {
   const setFormGradeStep = useSetFormGradeStepStore();
   const { saveFormMutate } = useSaveFormMutation();
   const { run: FormStep } = useFormStep();
+  const { toast } = useToast();
 
   const onFieldChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
     const { name, value } = e.target;
@@ -32,6 +34,7 @@ export const useIntoductionForm = () => {
       });
     } catch (err) {
       if (err instanceof z.ZodError) {
+        toast('금지어가 포함되어 있습니다.', 'ERROR');
         const fieldErrors = err.flatten().fieldErrors;
         const normalizedErrors = Object.fromEntries(
           Object.entries(fieldErrors).map(([key, value]) => [key, value ?? []])
--- apps/user/src/hooks/useGradeCaculation.ts
+++ apps/user/src/hooks/useGradeCaculation.ts
@@ -30,7 +30,7 @@ const useGradeCalculation = () => {
     const SEMESTER_WEIGHT = [0.2, 0.2, 0.3, 0.3];
 
     let total = 0;
-    
+
     form.grade.subjectList?.forEach((subject) => {
       const subjectWeight =
         SUBJECT_WEIGHT[subject.subjectName as keyof typeof SUBJECT_WEIGHT];
