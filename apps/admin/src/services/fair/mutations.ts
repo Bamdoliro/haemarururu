56271638c70a700ace86475595a5c2142421815d
junghongseop <a01082372487@gmail.com>
2025-10-30 10:28:54 +0900

    feat(admin): react query 작성

--- apps/admin/src/services/fair/mutations.ts
+++ apps/admin/src/services/fair/mutations.ts
@@ -1,10 +1,11 @@
 import { useMutation } from '@tanstack/react-query';
 import type { FairApiRequestBody } from '@/components/fair/FairForm/fair.hooks';
-import { postFairReq } from '@/services/fair/api';
+import { deleteFair, postFairReq, putFair } from '@/services/fair/api';
 import { useApiError } from '@/hooks';
 import { toast } from 'react-toastify';
 import { useRouter } from 'next/navigation';
 import { ROUTES } from '@/constants/common/constant';
+import type { PutFairReq } from '@/types/fair/remote';
 
 export const useCreateFairMutation = () => {
   const { handleError } = useApiError();
@@ -20,4 +21,40 @@ export const useCreateFairMutation = () => {
     },
     onError: handleError,
   });
+};
+
+export const usePutFairMutation = (fairId: number) => {
+  const { handleError } = useApiError();
+  const router = useRouter();
+
+  const { mutate: putFairMutate, ...restMutation } = useMutation({
+    mutationFn: (params: PutFairReq) => putFair(fairId, params),
+    onSuccess: () => {
+      toast('입학설명회가 수정되었습니다.', {
+        type: 'success',
+      });
+      router.push(ROUTES.FAIR);
+    },
+    onError: handleError,
+  });
+
+  return { putFairMutate, ...restMutation };
+};
+
+export const useDeleteFairMutation = (fairId: number) => {
+  const { handleError } = useApiError();
+  const router = useRouter();
+
+  const { mutate: deleteFairMutate, ...restMutation } = useMutation({
+    mutationFn: () => deleteFair(fairId),
+    onSuccess: () => {
+      toast('입학설명회가 삭제되었습니다.', {
+        type: 'success',
+      });
+      router.push(ROUTES.FAIR);
+    },
+    onError: handleError,
+  });
+
+  return { deleteFairMutate, ...restMutation };
 };
