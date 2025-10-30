55bf8041e6fa8032161963254f74a12bb83641bd
junghongseop <a01082372487@gmail.com>
2025-10-30 09:26:45 +0900

    fix(admin): 분석페이지 소수점으로 변경

--- apps/admin/src/components/analysis/GradeDistribution/GradeDistributionDetailTable/GradeDistributionDetailTable.hooks.ts
+++ apps/admin/src/components/analysis/GradeDistribution/GradeDistributionDetailTable/GradeDistributionDetailTable.hooks.ts
@@ -3,15 +3,15 @@ import type { GradeDistributionType } from '@/types/analysis/client';
 const useMaxMin = (formList: GradeDistributionType[] | undefined) => {
   const entireFirstRoundMax =
     formList && formList.length > 0
-      ? Math.max(...formList.map((item) => item.firstRoundMax)).toFixed(2)
-      : '0.00';
+      ? Math.max(...formList.map((item) => item.firstRoundMax)).toFixed(3)
+      : '0.000';
 
   const minList = formList
     ? formList.map((item) => item.firstRoundMin).filter((value) => value !== 0)
     : [];
 
   const entireFirstRoundMin =
-    minList.length > 0 ? Math.min(...minList).toFixed(2) : '0.00';
+    minList.length > 0 ? Math.min(...minList).toFixed(3) : '0.000';
 
   return { max: entireFirstRoundMax, min: entireFirstRoundMin };
 };
