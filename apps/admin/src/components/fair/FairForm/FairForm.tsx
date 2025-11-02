c761c45e3909d20916b204516a953f41555b7639
junghongseop <a01082372487@gmail.com>
2025-11-03 08:44:55 +0900

    fix(admin): 잘못된 상태관리 수정 및 정원 받는 인풋 추가

--- apps/admin/src/components/fair/FairForm/FairForm.tsx
+++ apps/admin/src/components/fair/FairForm/FairForm.tsx
@@ -1,17 +1,17 @@
 import { color } from '@maru/design-system';
-import { Text, RadioGroup } from '@maru/ui';
+import { Text, RadioGroup, Button } from '@maru/ui';
 import { flex } from '@maru/utils';
 import { styled } from 'styled-components';
 import { IconClock, IconCalendar } from '@maru/icon';
 import FormInput from '@maru/ui/src/Input/FormInput';
 import type { FairType } from '@/types/fair/client';
 import { useFairForm } from '@/components/fair/FairForm/fair.hooks';
-import { useFairFormStore } from '@/store/fair/fairType';
+import { useFairFormValueStore } from '@/store/fair/fairType';
 import formatDateInput from '@/utils/functions/formatDateInput';
 import formatTimeInput from '@/utils/functions/formatTimeInput';
 
 const FairForm = () => {
-  const [form] = useFairFormStore();
+  const fair = useFairFormValueStore();
 
   const {
     handleChange,
@@ -27,7 +27,7 @@ const FairForm = () => {
         <Text fontType="context">대상선택</Text>
         <RadioGroup
           name="type"
-          value={form.type}
+          value={fair.type}
           onChange={(e) => handleChange('type', e.target.value as FairType)}
           items={[
             { label: '학생/학부모', value: 'STUDENT_AND_PARENT' },
@@ -40,7 +40,7 @@ const FairForm = () => {
         <FormInput
           placeholder="장소를 입력해주세요."
           name="place"
-          value={form.place}
+          value={fair.place}
           onChange={handleInputChange}
         />
       </CreateFormSort>
@@ -50,7 +50,7 @@ const FairForm = () => {
           <FormInput
             placeholder="날짜를 입력해주세요"
             name="start-date"
-            value={formatDateInput(form.start.slice(0, 8))}
+            value={formatDateInput(fair.start.slice(0, 8))}
             onChange={(e) => handleDateChange(e.target.value)}
             inputMode="numeric"
           />
@@ -65,7 +65,7 @@ const FairForm = () => {
           <FormInput
             placeholder="시간을 입력해주세요."
             name="start-time"
-            value={formatTimeInput(form.start.slice(8))}
+            value={formatTimeInput(fair.start.slice(8))}
             onChange={(e) => handleTimeChange(e.target.value)}
           />
           <InputIconWrapper>
@@ -74,25 +74,34 @@ const FairForm = () => {
         </InputWrapper>
       </CreateFormSort>
       <CreateFormSort>
+        <Text fontType="context">신청 인원 (정원)</Text>
+        <FormInput
+          placeholder="인원을 입력해주세요."
+          name="capacity"
+          value={fair.capacity}
+          onChange={handleInputChange}
+        />
+      </CreateFormSort>
+      <CreateFormSort>
         <Text fontType="context">신청 기한 (8자리)</Text>
         <CreateInputSort>
           <FormInput
             placeholder="시작일"
             name="applicationStartDate"
-            value={formatDateInput(form.applicationStartDate ?? '')}
+            value={formatDateInput(fair.applicationStartDate ?? '')}
             onChange={handleInputChange}
           />
           <FormInput
             placeholder="종료일"
             name="applicationEndDate"
-            value={formatDateInput(form.applicationEndDate ?? '')}
+            value={formatDateInput(fair.applicationEndDate ?? '')}
             onChange={handleInputChange}
           />
         </CreateInputSort>
       </CreateFormSort>
-      <CreateFairButton onClick={handleSubmit}>
+      <Button onClick={handleSubmit} size="LARGE">
         <Text fontType="btn1">새로운 입학전형 설명회 생성하기</Text>
-      </CreateFairButton>
+      </Button>
     </StyledFairForm>
   );
 };
@@ -101,12 +110,16 @@ export default FairForm;
 
 const StyledFairForm = styled.div`
   width: 500px;
-  height: 703px;
+  height: 804px;
   background-color: ${color.gray50};
   border-radius: 12px;
   border: 1px solid ${color.gray250};
   padding: 56px 70px;
-  ${flex({ flexDirection: 'column', alignItems: 'center' })};
+  ${flex({
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  })};
 `;
 
 const CreateFormSort = styled.div`
@@ -124,14 +137,6 @@ const CreateInputSort = styled.div`
     flex: 1;
     max-width: 100%;
   }
-`;
-
-const CreateFairButton = styled.button`
-  background-color: ${color.haeMaruDefault};
-  color: ${color.white};
-  border-radius: 6px;
-  margin-top: 24px;
-  padding: 22px;
 `;
 
 const InputWrapper = styled.div`
