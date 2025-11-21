389935d98fb06da542da46e2605d31ef130aab47
hjy080530 <bomaye119@gmail.com>
2025-11-21 10:56:55 +0900

    chore(user): 일부 오타와 요구 사항 수정

--- apps/user/src/components/form/FinalFormConfirm/FinalFormConfirm.tsx
+++ apps/user/src/components/form/FinalFormConfirm/FinalFormConfirm.tsx
@@ -16,12 +16,12 @@ const FinalFormConfirm = ({ isOpen, onClose, onConfirm }: FinalFormConfirmProps)
       content={
         <Column gap={12}>
           <Text color={color.gray900} fontType="p2">
-            모든 제출서류를 업로드하셨다면 원서를 최종 제출 해주세요.
+            모든 제출서류를 업로드하셨다면 원서를 입력 완료 해주세요.
           </Text>
           <Text color={color.red} fontType="p2">
-            원서 최종 제출 시 입학원서 접수가 완료되며 더 이상 수정이 불가합니다.
+            원서 입력 완료 시 입학원서 접수가 완료되며 더 이상 수정이 불가합니다.
             <br />
-            최종 제출한 원서를 출력해서 우편, 또는 학교로 제출해 주시길 바랍니다.
+            입력 완료한 원서를 출력해서 우편, 또는 학교로 제출해 주시길 바랍니다.
           </Text>
         </Column>
       }
