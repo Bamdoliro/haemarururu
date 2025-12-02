82136c5778b1677b001e22460c1107ee7d616492
hjy080530 <bomaye119@gmail.com>
2025-12-02 11:05:26 +0900

    feat(user): 유틸 요일 추가

--- apps/user/src/utils/formatMonthDay.ts
+++ apps/user/src/utils/formatMonthDay.ts
@@ -1,10 +1,15 @@
 import dayjs, { type Dayjs } from 'dayjs';
 
-const formatMonthDay = (date: string | Dayjs): string => {
+const formatMonthDay = (date: string | Dayjs, type?: 'DAY_OF_WEEK'): string => {
   const day = dayjs(date);
-
   const m = day.month() + 1;
   const d = day.date();
+
+  if (type === 'DAY_OF_WEEK') {
+    const dayNames = ['일', '월', '화', '수', '목', '금', '토'];
+    const dayOfWeek = dayNames[day.day()];
+    return `${m}월 ${d}일(${dayOfWeek})`;
+  }
 
   return `${m}월 ${d}일`;
 };
