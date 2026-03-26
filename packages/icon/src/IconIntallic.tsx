d8bfd3a9442790101319a1385c742691f100913e
hjy080530 <bomaye119@gmail.com>
2026-03-26 09:06:15 +0900

    chore: 필요한 icon 추가

---
+++ packages/icon/src/IconIntallic.tsx
@@ -0,0 +1,25 @@
+import type { SVGProps } from 'react';
+import React from 'react';
+
+const IconIntallic = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M15 9H21M11 23H17M18 9L14 23"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default IconIntallic;
