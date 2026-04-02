32adc9f80b04f28065a6c1a4ca6dc70e91aa569d
hjy080530 <bomaye119@gmail.com>
2026-04-02 17:33:07 +0900

    chore(admin): img 태그를 사용해도 eslint 에러가 나지 않게 수정

--- apps/admin/.eslintrc.cjs
+++ apps/admin/.eslintrc.cjs
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@maru/eslint-config/react-ts-noimport', 'plugin:@next/next/core-web-vitals'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
+    '@next/next/no-img-element': 'off',
   },
 };
